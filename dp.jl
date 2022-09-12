49badaf8fd1b25732391e5f6255d5f8042a08539
Beryl A <berylramadhian@gmail.com>
2022-09-12 21:04:39 +0200

    Py DScribe works, now how to use it in Julia?

--- dp.jl
+++ dp.jl
@@ -1,13 +1,23 @@
-using JLD
+using Pandas, PyCall
 
 """
 usual main caller
 """
 function main()
     # read gdb file:
-    f = open("data/qm9/dsgdb9nsd_000002.xyz")
-    lines = readlines(f)
-    display(lines)
+    #f = open("data/qm9/dsgdb9nsd_000002.xyz")
+    #lines = readlines(f)
+    #display(lines)
+    # test load pickle from python
+    math = pyimport("math")
+    println(math.sin(math.pi / 4)) # returns ≈ 1/√2 = 0.70710678...
 end
 
 main()
+py"""
+import numpy as np
+
+def sinpi(x):
+    return np.sin(np.pi * x)
+"""
+py"sinpi"(1)
