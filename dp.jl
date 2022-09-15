e635b30b1ffda7a3b8e10785ac28854fc6ddfcf3
Beryl Aribowo <berylramadhian@gmail.com>
2022-09-15 12:29:17 +0200

    added molecular formula converter

--- dp.jl
+++ dp.jl
@@ -1,17 +1,51 @@
 
-using DelimitedFiles
+using DelimitedFiles, DataStructures
+
+
+"""
+generate molecular formula based on the list of atoms
+the formula is in the form of ordered atoms: H_C_N_O_F
+"""
+function generate_mol_formula(atoms)
+    c = counter(atoms)
+    out = ""
+    if c["H"] > 0
+        out *= "H"*string(c["H"])
+    end
+    if c["C"] > 0
+        out *= "C"*string(c["C"])
+    end
+    if c["N"] > 0
+        out *= "N"*string(c["N"])
+    end
+    if c["O"] > 0
+        out *= "O"*string(c["O"])
+    end
+    if c["F"] > 0
+        out *= "F"*string(c["F"])
+    end
+    return out
+end
 
 """
 usual main caller
 """
 function main()
     # read gdb file:
-    f = open("data/qm9/dsgdb9nsd_000002.xyz")
-    lines = readlines(f)
-    display(lines)
-    #006140 000001
-    fd = readdlm("data/qm9/dsgdb9nsd_000001.xyz", '\t', String, '\n')
-    display(fd[2, 1:16])
+    fd = readdlm("data/qm9/dsgdb9nsd_022908.xyz", '\t', String, '\n')
+    n_atom = parse(Int64, fd[1,1])
+    atoms = fd[3:3+n_atom-1, 1]
+    energy = parse(Float64, fd[2, 13])
+    coords = parse.(Float64, fd[3:3+n_atom-1, 2:4])
+    formula = generate_mol_formula(atoms)
+    println(n_atom)
+    println(formula)
+    data = Dict("")
+    display(coords)
+    println(energy)
+
+    
+
 end
 
 main()
