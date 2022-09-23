f5e343655e3276e898632612a0acf5d75d1c57b2
Beryl Aribowo <berylramadhian@gmail.com>
2022-09-23 17:11:22 +0200

    added and semi-tested Bspline (translated from matlab)

--- RoSemi.jl
+++ RoSemi.jl
@@ -1,4 +1,4 @@
-using JLD
+using JLD, SparseArrays
 
 include("voronoi.jl")
 include("linastic.jl")
@@ -7,7 +7,37 @@ include("linastic.jl")
 placeholder for the (Ro)bust (S)h(e)pard (m)odel for (i)nterpolation constructor
 """
 
-function test()
+
+"""
+bspline constructor (mimic of matlab ver by prof. Neumaier)
+params:
+    - x, feature matrix (ndata x nfeature)
+"""
+function bspline(x)
+    m, n = size(x)
+    s = sparse(zeros(m, n))
+    x = abs.(x)
+    ind = x .< 1
+    x1 = x[ind]
+    s[ind] = 1 .+ .75*x1.^2 .*(x1 .- 2)
+    ind = (s .< 2) .&& (.~ind)
+    x1 = x[ind]
+    s[ind] = .25*(2 .- x1).^3
+    return s
+end
+
+function test_spline()
+    M = 3
+    n_data = Integer(1e3)
+    x = reshape(collect(LinRange(-1., 1., n_data)), 1, :) # data points with just 1 feature, matrix(1, ndata)
+    S = Matrix{Float64}(undef, n_data, M+1)
+    for i ∈ 0:M
+        S[:, i+1] = bspline(M.*x .- i)
+    end
+    plot(vec(x), S)
+end
+
+function test_cluster()
     # load dataset || the datastructure is subject to change!! since the data size is huge
     dataset = load("data/ACSF_1000_symm.jld")["data"]
     N, L = size(dataset)
