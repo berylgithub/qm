e1b8dfb29d735bc74810c6f03a476772af922fa7
Beryl Aribowo <berylramadhian@gmail.com>
2022-09-30 17:01:22 +0200

    tested FD vs RD for Jac of LS, FD is much much faster

--- exp.jl
+++ exp.jl
@@ -1,3 +1,4 @@
+using LsqFit
 """
 contains all tests and experiments
 """
@@ -9,12 +10,26 @@ include("RoSemi.jl")
 
 
 """
-compute the basis functions from normalized data
+compute the basis functions from normalized data and assemble A matrix:
 """
 function main_basis()
     W = load("data/ACSF_1000_symm_scaled.jld")["data"]'# load normalized fingerprint
     M = 10
-    ϕ = extract_bspline(W, M) # compute basis from fingerprint
+    ϕ = extract_bspline(W, M) # compute basis from fingerprint ∈ (n_feature, n_data, M+3)
     #ϕ = sparse(ϕ[:,:,13]) # sparse can only take matrix
+    # assemble matrix A:
+end
+
+
+"""
+test for linear system fitting using leastsquares
+"""
+function test_lsfit()
+    ndata = 3; nfeature=5
+    A = rand(ndata, nfeature)
+    θ = rand(nfeature)
+    b = ones(ndata)
+    curve_fit()
     
 end
+
