dc4bb39ca34dd9f26d2a7f2cf050fe9f67ea8332
Beryl Aribowo <berylramadhian@gmail.com>
2022-09-30 18:59:52 +0200

    the closure function should be defined inside the function s.t. the function can read the parametersgit add exp.jl !

--- exp.jl
+++ exp.jl
@@ -1,4 +1,4 @@
-using LsqFit, ReverseDiff, ForwardDiff, BenchmarkTools
+using LsqFit, ReverseDiff, ForwardDiff, BenchmarkTools, Optim
 """
 contains all tests and experiments
 """
@@ -27,20 +27,20 @@ test for linear system fitting using leastsquares
 NOTES:
     - for large LSes, ForwardDiff is much faster for jacobian but ReverseDiff is much faster for gradient !!
 """
-function test_AD()
-    ndata = Int(1e4); nfeature=Int(1e4)
-    #ndata = 3; nfeature=3
-    #A = Matrix{Float64}(LinearAlgebra.I, 3,3)
-    A = rand(ndata, nfeature)
-    display(A)
+function test_fit()
+    #ndata = Int(1e4); nfeature=Int(1e4)
+    ndata = 3; nfeature=3
+    A = Matrix{Float64}(LinearAlgebra.I, 3,3)
+    #A = rand(ndata, nfeature)
+    #display(A)
     θ = rand(nfeature)
-    b = ones(ndata)
+    b = ones(ndata) .+ 10.
     r = residual(A, θ, b)
-    t = @elapsed begin
-        dθ = ReverseDiff.gradient(θ -> lsq(A, θ, b), θ)
+    display(r)
+    function df!(g, θ)
+        g .= ReverseDiff.gradient(θ -> lsq(A, θ, b), θ)
     end
-    display(dθ)
-    display(t)
-    #fit = curve_fit()
+    res = optimize(θ -> lsq(A, θ, b), df!, θ, LBFGS())
+    display(Optim.minimizer(res))
 end
 
