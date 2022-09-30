ca24e50be57f3065f06c75cc0b524a0a87058a72
Beryl Aribowo <berylramadhian@gmail.com>
2022-09-30 15:20:57 +0200

    sparse can only take matrix (atleast for now this is the only known thing)

--- exp.jl
+++ exp.jl
@@ -2,8 +2,19 @@
 contains all tests and experiments
 """
 
-include("dp.jl")
-include("molfinger.jl")
 include("voronoi.jl")
 include("linastic.jl")
 include("RoSemi.jl")
+
+
+
+"""
+compute the basis functions from normalized data
+"""
+function main_basis()
+    W = load("data/ACSF_1000_symm_scaled.jld")["data"]'# load normalized fingerprint
+    M = 10
+    ϕ = extract_bspline(W, M) # compute basis from fingerprint
+    #ϕ = sparse(ϕ[:,:,13]) # sparse can only take matrix
+    
+end
