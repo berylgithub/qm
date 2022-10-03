9a6311dc0efe38b07b2b387004283d16496e51b8
berylramadhian <berylramadhian@gmail.com>
2022-10-03 13:18:42 +0200

    added basic comp funcs for the matrix assemhbly

--- RoSemi.jl
+++ RoSemi.jl
@@ -7,6 +7,24 @@ include("linastic.jl")
 placeholder for the (Ro)bust (S)h(e)pard (m)odel for (i)nterpolation constructor
 """
 
+"""
+mostly unused (although faster), verbose version
+"""
+function bspline2(x)
+    m, n = size(x) # fingerprint x data
+    β = sparse(zeros(m, n))
+    for j ∈ 1:n 
+        for i ∈ 1:m
+            z = abs(x[i,j])
+            if z < 1
+                β[i,j] = 1 + .75*x[i,j]^2 * (z - 2)
+            elseif 1 ≤ z < 2
+                β[i,j] = 0.25 * (2 - z)^3
+            end
+        end
+    end
+    return β
+end
 
 """
 The Bspline works for matrices
@@ -29,26 +47,6 @@ function bspline(z)
 end
 
 
-
-"""
-verbose version
-"""
-function bspline2(x)
-    m, n = size(x) # fingerprint x data
-    β = sparse(zeros(m, n))
-    for j ∈ 1:n 
-        for i ∈ 1:m
-            z = abs(x[i,j])
-            if z < 1
-                β[i,j] = 1 + .75*x[i,j]^2 * (z - 2)
-            elseif 1 ≤ z < 2
-                β[i,j] = 0.25 * (2 - z)^3
-            end
-        end
-    end
-    return β
-end
-
 """
 wrapper to extract M+3 or n_basis amount of splines
 params:
@@ -67,15 +65,43 @@ function extract_bspline(x, M)
 end
 
 """
-refer to RoSeMI.pdf for these quantities:
+=============================
+refer to RoSeMI.pdf and RSI.pdf for these quantities:
 """
-function compute_γk()
-    
+
+"""
+get the distance between w_l and w_k, D_k(w_l), uses precomputed matrix D with fixed i
+params:
+    - D, mahalanobis distance matrix, ∈ Float64 (n_data, n_data)
+    - l, index of the selected unsupervised datapoint
+    - k, index of the selected supervised datapoint
+"""
+function get_Dk(D, k, l)
+    return D[k, l] # = D[l,k], same thing    
 end
 
-function compute_αj()
-    
+"""
+compute S_K := ∑1/Dₖ
+params:
+    - D, mahalanobis distance matrix, ∈ Float64 (n_data, n_data)
+    - l, index of the selected unsupervised datapoint
+"""
+function comp_SK(D, l)
+    return sum(D[:,l])
 end
+
+function comp_γk(Dk, SK)
+    return Dk*SK
+end
+
+function comp_αj(Dj, SK)
+    return Dj*SK - 1
+end
+
+
+"""
+==================================
+"""
 
 
 function test_spline()
@@ -92,49 +118,6 @@ function test_spline()
     end
 end
 
-function test_cluster()
-    # load dataset || the datastructure is subject to change!! since the data size is huge
-    dataset = load("data/ACSF_1000_symm.jld")["data"]
-    N, L = size(dataset)
-    A = dataset' # transpose data (becomes column major)
-    display(A)
-    println(N, " ", L)
-    M = 10 # number of selected data
-    # compute mean and cov:
-    idx = 603 # the ith data point of the dataset, can be arbitrary technically
-    wbar, C = mean_cov(A, idx, N, L)
-    B = compute_B(C)
-    display(wbar)
-    display(B)
-    # generate centers (M) for training:
-    center_ids, mean_point = eldar_cluster(A, M, wbar=wbar, B=B, distance="mahalanobis", mode="fmd") # generate cluster centers
-    display(mean_point)
-    display(center_ids)
-end
-
-
-"""
-compute all D_k(w_l) ∀k,l, for now fix i = 603 (rozemi)
-"""
-function test_all_dist()
-    dataset = load("data/ACSF_1000_symm.jld")["data"]
-    N, L = size(dataset)
-    W = dataset' # transpose data (becomes column major)
-    println(N, " ", L)
-    M = 10 # number of selected data
-    # compute mean and cov:
-    idx = 603 # the ith data point of the dataset, can be arbitrary technically (for now fix i=603)
-    wbar, C = mean_cov(W, idx, N, L)
-    B = compute_B(C)
-    #display(wbar)
-    #dist = f_distance(B, A[:,1], A[:,2])
-    #display(dist)
-
-    # compute all distances:
-    filename = "data/distances_1000_i=603.jld"
-    D = compute_distance_all(W, B, filename)
-    display(D)
-end
 
 """
 test extract basis from data
