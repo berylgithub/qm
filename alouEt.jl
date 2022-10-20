2d0a6c9c3bb70af8a250b0d53d12fa4de525e783
berylramadhian <berylramadhian@gmail.com>
2022-10-20 19:25:53 +0200

    try PCA -> slice -> normalize, not any better

--- alouEt.jl
+++ alouEt.jl
@@ -84,9 +84,12 @@ function data_setup(mol_name, n_data, n_feature, M; universe_size=1_000)
         println(length(D))
         save(path*"/$mol_name"*"_dataset_$n_data.jld", "data", D)
         # slice the global feature matrix:
-        #W = load("data/ACSF_PCA$n_feature"*"_scaled_1.jld")["data"] # try with scaled(PCA(W)), this file is pre-generated 🌸
-        W = load("data/ACSF_PCA_scaled.jld")["data"] # load scaled(PCA(features)), this is more accurate since the columns are sorted by the most important featuers
-        W = W[indexes, 1:n_feature] # slice the featuere matrix by the data indices and the first n_feature
+        #W = load("data/ACSF_PCA_scaled.jld")["data"] # load scaled(PCA(features)), this is more accurate since the columns are sorted by the most important featuers
+        #W = W[indexes, 1:n_feature] # slice the featuere matrix by the data indices and the first n_feature
+        # try PCA -> slice -> normalize:
+        W = load("data/ACSF_PCA.jld")["data"]   # PCA
+        W = W[indexes, 1:n_feature]     # slice
+        W = normalize_routine(W)        # normalize
         main_file = path*"/$mol_name"*"_ACSF_"*"$n_feature"*"_"*"$n_data.jld"
         save(main_file, "data", W)
         # get center indexes:
