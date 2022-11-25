7bb156a1cac28e45bc0d929a059659bbe815a81a
berylramadhian <berylramadhian@gmail.com>
2022-11-25 18:43:59 +0100

    new experiments, hope no out of memories

--- solaeter.jl
+++ solaeter.jl
@@ -1,25 +1,23 @@
 include("alouEt.jl")
 
 function caller()
-    #= data_setup("exp_all_1", 1:133728, 4, 26, 2, 300, "data/ACSF_atom.jld"; ft_sos=false, ft_bin=false); fit_🌹("exp_all_1", 10_000, 900);
-    data_setup("exp_all_1", 1:133728, 5, 28, 2, 300, "data/ACSF_atom.jld"; ft_sos=false, ft_bin=false); fit_🌹("exp_all_1", 10_000, 900);
-    data_setup("exp_all_1", 1:133728, 6, 28, 2, 300, "data/ACSF_atom.jld"; ft_sos=false, ft_bin=false); fit_🌹("exp_all_1", 10_000, 900);
-    # with sums of squares:
-    data_setup("exp_all_1", 1:133728, 4, 28, 2, 300, "data/ACSF_atom.jld"; ft_sos=true, ft_bin=false); fit_🌹("exp_all_1", 10_000, 900);
-    data_setup("exp_all_1", 1:133728, 5, 28, 2, 300, "data/ACSF_atom.jld"; ft_sos=true, ft_bin=false); fit_🌹("exp_all_1", 10_000, 900);
-    data_setup("exp_all_1", 1:133728, 6, 28, 2, 300, "data/ACSF_atom.jld"; ft_sos=true, ft_bin=false); fit_🌹("exp_all_1", 10_000, 900);
-    # with sums of squares and binomial:
-    data_setup("exp_all_1", 1:133728, 4, 28, 2, 300, "data/ACSF_atom.jld"; ft_sos=true, ft_bin=true); fit_🌹("exp_all_1", 10_000, 900);
-    data_setup("exp_all_1", 1:133728, 5, 28, 2, 300, "data/ACSF_atom.jld"; ft_sos=true, ft_bin=true); fit_🌹("exp_all_1", 10_000, 900);
-    data_setup("exp_all_1", 1:133728, 6, 28, 2, 300, "data/ACSF_atom.jld"; ft_sos=true, ft_bin=true); fit_🌹("exp_all_1", 10_000, 900); =#
-
-    data_setup("exp_all_1", 1:133728, 4, 20, 2, 300, "data/ACSF_atom.jld"; ft_sos=false, ft_bin=false); fit_🌹("exp_all_1", 10_000, 900);
-    data_setup("exp_all_1", 1:133728, 4, 20, 2, 300, "data/ACSF_atom.jld"; ft_sos=false, ft_bin=false); fit_🌹("exp_all_1", 10_000, 900);
-    data_setup("exp_all_1", 1:133728, 4, 26, 2, 300, "data/ACSF_atom.jld"; ft_sos=false, ft_bin=false); fit_🌹("exp_all_1", 10_000, 900);
-    data_setup("exp_all_1", 1:133728, 6, 25, 2, 300, "data/ACSF_atom.jld"; ft_sos=false, ft_bin=false); fit_🌹("exp_all_1", 10_000, 900);
-    data_setup("exp_all_1", 1:133728, 6, 32, 2, 300, "data/ACSF_atom.jld"; ft_sos=false, ft_bin=false); fit_🌹("exp_all_1", 10_000, 900);
-    data_setup("exp_all_1", 1:133728, 5, 36, 2, 300, "data/ACSF_atom.jld"; ft_sos=true, ft_bin=false); fit_🌹("exp_all_1", 10_000, 900);
-    data_setup("exp_all_1", 1:133728, 5, 50, 2, 300, "data/ACSF_atom.jld"; ft_sos=true, ft_bin=true); fit_🌹("exp_all_1", 10_000, 900);
-
+    data_setup("exp_all_1", 1:133728, 4, 18, 2, 300, "data/ACSF_atom.jld"; ft_sos=false, ft_bin=false); fit_🌹("exp_all_1", 10_000, 900); # warm start
+    nmf = 18:26; n_bas = 2:8
+    for i ∈ nmf
+        for j ∈ n_bas
+            data_setup("exp_all_1", 1:133728, 4, i, j, 300, "data/ACSF_atom.jld"; ft_sos=false, ft_bin=false); fit_🌹("exp_all_1", 10_000, 900);
+        end
+    end
+    nmf = 21:30; n_bas = 2:5
+    for i ∈ nmf
+        for j ∈ n_bas
+            data_setup("exp_all_1", 1:133728, 4, i, j, 300, "data/ACSF_atom.jld"; ft_sos=true, ft_bin=false); fit_🌹("exp_all_1", 10_000, 900);
+        end
+    end
+    for i ∈ nmf
+        for j ∈ n_bas
+            data_setup("exp_all_1", 1:133728, 4, i, j, 300, "data/ACSF_atom.jld"; ft_sos=true, ft_bin=true); fit_🌹("exp_all_1", 5_000, 900);
+        end
+    end
     println("fit experiments complete!!")
 end
