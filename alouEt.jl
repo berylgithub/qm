44e71d61d808a487fa99e48de2a7bb7138790cd3
berylramadhian <berylramadhian@gmail.com>
2022-11-25 11:58:45 +0100

    added data setup for molecular file directly

--- alouEt.jl
+++ alouEt.jl
@@ -215,17 +215,24 @@ end
 """
 full data setup, in contrast to each molecule data setup, INCLUDES PCA!.
 takes in the data indices (relative to the qm9 dataset).
+if molf_file  is not empty then there will be no atomic feature extractions, only PCA on molecular level
 """
-function data_setup(foldername, data_indices, n_af, n_mf, n_basis, num_centers, feature_file; universe_size=1_000, ft_sos=true, ft_bin=true)
+function data_setup(foldername, data_indices, n_af, n_mf, n_basis, num_centers, feature_file; universe_size=1_000, ft_sos=true, ft_bin=true, molf_file = "")
     println("data setup for n_data = ",length(data_indices),", atom features = ",n_af, ", mol features = ", n_mf, ", centers = ",num_centers, " starts!")
     t = @elapsed begin
         path = mkpath("data/$foldername")
         # load dataset:
         dataset = load("data/qm9_dataset.jld")["data"]
         # PCA:
-        F = load(feature_file)["data"] # pre-extracted atomic features
+        F = nothing
         plot_fname = "$foldername"*"_$n_af"*"_$n_mf"*"_$ft_sos"*"_$ft_bin" # plot name infix
-        F = feature_extractor(F, dataset, n_af, n_mf, ft_sos=ft_sos, ft_bin=ft_bin, fname_plot_at=plot_fname, fname_plot_mol=plot_fname)
+        if length(molf_file) == 0 # if molecular feature file is not provided:
+            F = load(feature_file)["data"] # pre-extracted atomic features
+            F = feature_extractor(F, dataset, n_af, n_mf, ft_sos=ft_sos, ft_bin=ft_bin, fname_plot_at=plot_fname, fname_plot_mol=plot_fname)
+        else
+            F = load(molf_file)["data"]
+            F = PCA_mol(F, n_mf, fname_plot_mol = molf_file)
+        end
         F = F[data_indices, :]
         dataset = dataset[data_indices] # slice dataset
         display(dataset)
