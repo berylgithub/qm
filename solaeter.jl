258d4335eafbaf09c3c886a003f10e8a813e39e9
berylramadhian <berylramadhian@gmail.com>
2022-11-28 16:36:04 +0100

    more param change

--- solaeter.jl
+++ solaeter.jl
@@ -5,21 +5,21 @@ function caller()
     nmf = 23:26; n_bas = 5:8
     for i ∈ nmf
         for j ∈ n_bas
-           if i <= 23 && j <= 5
-	      continue
-	   else
-              data_setup("exp_all_1", 1:133728, 4, i, j, 300, "data/ACSF_atom.jld"; ft_sos=false, ft_bin=false); fit_🌹("exp_all_1", 10_000, 900);
-    	   end
+            if i <= 25 && j <= 6
+	            continue
+	        else
+                data_setup("exp_all_1", 1:133728, 4, i, j, 300, "data/ACSF_atom.jld"; ft_sos=false, ft_bin=false); fit_🌹("exp_all_1", 10_000, 900);
+    	    end
         end
     end
     nmf = 21:30; n_bas = 2:5
     for i ∈ nmf
         for j ∈ n_bas
-	   if i <= 23 && j <= 6
-	      continue
-	   else
-              data_setup("exp_all_1", 1:133728, 4, i, j, 300, "data/ACSF_atom.jld"; ft_sos=true, ft_bin=false); fit_🌹("exp_all_1", 10_000, 900);
-	   end
+	        if i <= 23 && j <= 6
+	            continue
+	        else
+                data_setup("exp_all_1", 1:133728, 4, i, j, 300, "data/ACSF_atom.jld"; ft_sos=true, ft_bin=false); fit_🌹("exp_all_1", 10_000, 900);
+	        end
         end
     end
     for i ∈ nmf
