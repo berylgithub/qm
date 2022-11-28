2d0c743e154a465483fdb12fc84e338ab29a7c2c
Beryl Aribowo <berylramadhian@gmail.com>
2022-11-28 12:39:45 +0100

    bugfix index skips

--- solaeter.jl
+++ solaeter.jl
@@ -5,15 +5,21 @@ function caller()
     nmf = 20:26; n_bas = 5:8
     for i ∈ nmf
         for j ∈ n_bas
-	    if i <= 20 && j <= 7
-            	data_setup("exp_all_1", 1:133728, 4, i, j, 300, "data/ACSF_atom.jld"; ft_sos=false, ft_bin=false); fit_🌹("exp_all_1", 10_000, 900);
-	    end
+           if i <= 23 && j <= 8
+	      continue
+	   else
+              data_setup("exp_all_1", 1:133728, 4, i, j, 300, "data/ACSF_atom.jld"; ft_sos=false, ft_bin=false); fit_🌹("exp_all_1", 10_000, 900);
+    	   end
         end
     end
     nmf = 21:30; n_bas = 2:5
     for i ∈ nmf
         for j ∈ n_bas
-            data_setup("exp_all_1", 1:133728, 4, i, j, 300, "data/ACSF_atom.jld"; ft_sos=true, ft_bin=false); fit_🌹("exp_all_1", 10_000, 900);
+	   if i <= 23 && j <= 6
+	      continue
+	   else
+              data_setup("exp_all_1", 1:133728, 4, i, j, 300, "data/ACSF_atom.jld"; ft_sos=true, ft_bin=false); fit_🌹("exp_all_1", 10_000, 900);
+	   end
         end
     end
     for i ∈ nmf
