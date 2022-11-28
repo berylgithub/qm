429b6989c74cc6f066eac742d3d01f7aea58696b
berylramadhian <berylramadhian@gmail.com>
2022-11-28 13:05:25 +0100

    fixed indexing bug again

--- solaeter.jl
+++ solaeter.jl
@@ -2,10 +2,10 @@ include("alouEt.jl")
 
 function caller()
     #data_setup("exp_all_1", 1:133728, 4, 18, 2, 300, "data/ACSF_atom.jld"; ft_sos=false, ft_bin=false); fit_🌹("exp_all_1", 10_000, 900); # warm start
-    nmf = 20:26; n_bas = 5:8
+    nmf = 23:26; n_bas = 5:8
     for i ∈ nmf
         for j ∈ n_bas
-           if i <= 23 && j <= 8
+           if i <= 23 && j <= 5
 	      continue
 	   else
               data_setup("exp_all_1", 1:133728, 4, i, j, 300, "data/ACSF_atom.jld"; ft_sos=false, ft_bin=false); fit_🌹("exp_all_1", 10_000, 900);
