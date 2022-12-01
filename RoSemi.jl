1f230b2a79196c22d83ef33db3050f97ada831dd
berylramadhian <berylramadhian@gmail.com>
2022-12-01 20:09:02 +0100

    checkpoint tester KRR gaussian

--- RoSemi.jl
+++ RoSemi.jl
@@ -719,7 +719,7 @@ function comp_gaussian(x, σ)
     return exp.(-x / (2*σ^2))
 end
 
-# this is also used for prediction
+# this is also used for prediction, custom, with σ^2 instead of σ
 function comp_gaussian_elem(f1, f2, σ2)
     return exp(-norm(f1 - f2)^2 / (2*σ2) )
 end
