5ab5df93f18b50b83e52db22a0eabfec914be691
Beryl Aribowo <berylramadhian@gmail.com>
2022-12-12 17:41:46 +0100

    try training using cuda

--- schnet.py
+++ schnet.py
@@ -1,8 +1,131 @@
+import os
+import schnetpack as spk
 from schnetpack.datasets import QM9
 
-qm9data = QM9('./qm9.db', load_only=[QM9.U0], remove_uncharacterized=True)
+
+qm9tut = './qm9tut'
+if not os.path.exists('qm9tut'):
+    os.makedirs(qm9tut)
+
+qm9data = QM9('./qm9.db', load_only=[QM9.U0], remove_uncharacterized=True) # already pre-downloaded
+
+train, val, test = spk.train_test_split(
+        data=qm9data,
+        num_train=1000,
+        num_val=500,
+        split_file=os.path.join(qm9tut, "split.npz"),
+    )
+
+print(len(test))
+
+train_loader = spk.AtomsLoader(train, batch_size=100, shuffle=True)
+val_loader = spk.AtomsLoader(val, batch_size=100)
 
 atomrefs = qm9data.get_atomref(QM9.U0)
 print('U0 of hyrogen:', '{:.2f}'.format(atomrefs[QM9.U0][1][0]), 'eV')
 print('U0 of carbon:', '{:.2f}'.format(atomrefs[QM9.U0][6][0]), 'eV')
 print('U0 of oxygen:', '{:.2f}'.format(atomrefs[QM9.U0][8][0]), 'eV')
+
+
+# check energy:
+print(qm9data.get_properties(0)[1]["energy_U0"])
+
+means, stddevs = train_loader.get_statistics(
+    QM9.U0, divide_by_atoms=True, single_atom_ref=atomrefs
+)
+print('Mean atomization energy / atom:', means[QM9.U0])
+print('Std. dev. atomization energy / atom:', stddevs[QM9.U0])
+
+schnet = spk.representation.SchNet(
+    n_atom_basis=30, n_filters=30, n_gaussians=20, n_interactions=5,
+    cutoff=4., cutoff_network=spk.nn.cutoff.CosineCutoff
+)
+
+output_U0 = spk.atomistic.Atomwise(n_in=30, atomref=atomrefs[QM9.U0], property=QM9.U0,
+                                   mean=means[QM9.U0], stddev=stddevs[QM9.U0])
+model = spk.AtomisticModel(representation=schnet, output_modules=output_U0)
+
+
+# optimization:
+from torch.optim import Adam
+
+# loss function
+def mse_loss(batch, result):
+    diff = batch[QM9.U0]-result[QM9.U0]
+    err_sq = torch.mean(diff ** 2)
+    return err_sq
+
+# build optimizer
+optimizer = Adam(model.parameters(), lr=1e-2)
+
+import schnetpack.train as trn
+
+loss = trn.build_mse_loss([QM9.U0])
+
+metrics = [spk.metrics.MeanAbsoluteError(QM9.U0)]
+hooks = [
+    trn.CSVHook(log_path=qm9tut, metrics=metrics),
+    trn.ReduceLROnPlateauHook(
+        optimizer,
+        patience=5, factor=0.8, min_lr=1e-6,
+        stop_after_min=True
+    )
+]
+
+trainer = trn.Trainer(
+    model_path=qm9tut,
+    model=model,
+    hooks=hooks,
+    loss_fn=loss,
+    optimizer=optimizer,
+    train_loader=train_loader,
+    validation_loader=val_loader,
+)
+
+device = "cuda" # change to 'cpu' if gpu is not available
+n_epochs = 200 # takes about 10 min on a notebook GPU. reduces for playing around
+trainer.train(device=device, n_epochs=n_epochs)
+
+# plot training:
+import numpy as np
+import matplotlib.pyplot as plt
+from ase.units import kcal, mol
+
+results = np.loadtxt(os.path.join(qm9tut, 'log.csv'), skiprows=1, delimiter=',')
+
+time = results[:,0]-results[0,0]
+learning_rate = results[:,1]
+train_loss = results[:,2]
+val_loss = results[:,3]
+val_mae = results[:,4]
+
+print('Final validation MAE:', np.round(val_mae[-1], 2), 'eV =',
+      np.round(val_mae[-1] / (kcal/mol), 2), 'kcal/mol')
+
+# test model:
+import torch
+best_model = torch.load(os.path.join(qm9tut, 'best_model'))
+
+test_loader = spk.AtomsLoader(test, batch_size=100)
+
+err = 0
+print(len(test_loader))
+for count, batch in enumerate(test_loader):
+    # move batch to GPU, if necessary
+    batch = {k: v.to(device) for k, v in batch.items()}
+
+    # apply model
+    pred = best_model(batch)
+
+    # calculate absolute error
+    tmp = torch.sum(torch.abs(pred[QM9.U0]-batch[QM9.U0]))
+    tmp = tmp.detach().cpu().numpy() # detach from graph & convert to numpy
+    err += tmp
+
+    # log progress
+    percent = '{:3.2f}'.format(count/len(test_loader)*100)
+    print('Progress:', percent+'%'+' '*(5-len(percent)), end="\r")
+
+err /= len(test)
+print('Test MAE', np.round(err, 2), 'eV =',
+      np.round(err / (kcal/mol), 2), 'kcal/mol')
