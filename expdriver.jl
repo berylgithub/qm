508bfa03bee2ccd35aaedd7ac46fbf85c999890b
berylramadhian <berylramadhian@gmail.com>
2023-01-18 16:13:34 +0100

    untrack expdriver, since this will be edited a lot

--- expdriver.jl
+++
@@ -1,9 +0,0 @@
-include("alouEt.jl")
-
-function caller()
-    data_setup("exp_reduced_energy", 1:133728, 95, 22, 3, 300, "data/qm9_dataset_old.jld", "data/SOAP.jld", "SOAP")
-    models = ["ROSEMI", "KRR", "NN", "LLS", "GAK"]
-    for model ∈ models
-        fit_🌹_and_atom("exp_reduced_energy", 10_000, 900; model = model)
-    end
-end
