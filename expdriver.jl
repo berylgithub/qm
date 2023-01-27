45d332f00717c4b109447e88cb047c169dff98bf
berylramadhian <berylramadhian@gmail.com>
2023-01-27 21:03:27 +0100

    added driver, for convenience

---
+++ expdriver.jl
@@ -0,0 +1,45 @@
+include("alouEt.jl")
+
+function caller()
+    # FEEATURE EXTRACTION:
+    foldername = "exp_reduced_energy"
+    # ACSF:
+    nafs = [40, 30, 20] 
+    nmfs = [40, 30, 20]
+    println("ACSF")
+    for i ∈ eachindex(nafs)
+        for j ∈ eachindex(nmfs)
+            nmf = nmfs[j]
+            if nmf >= nafs[i] # the num of mol feature must be less than atom f
+                nmf = naf - rand(1:5, 1)[1]
+            end
+            println(nafs[i]," ",nmf)
+            data_setup(foldername, nafs[i], nmf, 3, 300, "data/qm9_dataset_old.jld", "data/ACSF_atom.jld", "ACSF"; 
+                        save_global_centers = true, num_center_sets = 5)
+        end
+    end
+    # SOAP:
+    nafs = [75, 50, 25]
+    nmfs = [75, 50, 25]
+    println("SOAP")
+    for i ∈ eachindex(nafs)
+        for j ∈ eachindex(nmfs)
+            nmf = nmfs[j]
+            if nmf >= nafs[i] # the num of mol feature must be less than atom f
+                nmf = naf - rand(1:5, 1)[1]
+            end
+            println(nafs[i]," ",nmf)
+            data_setup(foldername, nafs[i], nmf, 3, 300, "data/qm9_dataset_old.jld", "data/SOAP.jld", "SOAP"; 
+                        save_global_centers = true, num_center_sets = 5)
+        end
+    end
+
+    # ATOM FITTING:
+    uids = readdlm("data/centers.txt")[:, 1]
+    kids = readdlm("data/centers.txt")[:, 2]
+    center_sets = Int.(readdlm("data/centers.txt")[:, 3:end]) # a matrix (n_sets, n_centers) ∈ Int
+    for i ∈ eachindex(uids)
+        fit_atom("exp_reduced_energy", "data/qm9_dataset_old.jld", "data/atomref_features.jld";
+              center_ids=center_sets[i], uid=uids[i], kid=kids[i])
+    end
+end
