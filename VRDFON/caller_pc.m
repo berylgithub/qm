85ef1f9c8b00c7f69f5fe57fbd0c19ab85a21820
berylramadhian <berylramadhian@gmail.com>
2023-03-09 20:50:04 +0100

    unit tested new projection and rounding algorithm

--- VRDFON/caller_pc.m
+++ VRDFON/caller_pc.m
@@ -6,13 +6,44 @@
 %naf, nmf in percentage, e.g., 50 -> .5*max(naf("ACSF"))
 %feature name: 1=ACSF, 2=SOAP, 3=FCHL
 %model: ["ROSEMI", "KRR", "NN", "LLS", "GAK"]
-x = [.5, .5, 3, 1, 0, 0, 0, 6, 32.0]' %  example
-feas=paramcheck(x)
+x = [.5, .5, 3, 1, 0, 0, 0, -6, -2048.0]' %  example
 path_fun = '../data/hyperparamopt/fun.txt';
 path_fun = '../test.txt';
 path_bounds = '../data/hyperparamopt/bounds.txt';
 bounds = dlmread(path_bounds)
 
-%x = [-0.53389,0.17588,2,2,1,0,37,2,-30]'
-%x = parambound(x, bounds)
+%{
+i=8;
+lb = bounds(1,i)/(1+abs(bounds(1,i))); ub = bounds(2,i)/(1+abs(bounds(2,i)));
+bs = [ub, lb]
+bs = sort(bs)
+z = max(bs(1), min(x(i), bs(2)))
+z = z/(1-z)
+%}
 
+for i = 1:length(x)
+    tr = paramcheck(bounds(3,i), bounds(1,i), bounds(2,i)) % check whether need to be transformed or not
+    if bounds(3,i) == 1 % check type, int = 1, real = 0
+        x(i) = max(bounds(1,i), min(x(i), bounds(2,i))); % project to bounds
+        % stochastic round:
+        p = rand(1);
+        xl = floor(x(i));
+        f = x(i) - xl;
+        if p < 1-f; % larger chance to be rounded to the closest int
+            x(i) = xl;
+        else
+            x(i) = ceil(x(i));
+        end
+    else
+        if tr
+            lb = bounds(1,i)/(1+abs(bounds(1,i))); ub = bounds(2,i)/(1+abs(bounds(2,i))); % transform bounds
+            bs = [lb, ub]; bs = sort(bs); % transform bounds
+            z = max(bs(1), min(x(i), bs(2))); % clip to transformed bounds
+            x(i) = z/(1-z);
+        else
+            x(i) = max(bounds(1,i), min(x(i), bounds(2,i))); % project to bounds
+        end
+    end
+end
+
+disp(x)
--- VRDFON/xgenerator.m
+++ VRDFON/xgenerator.m
@@ -6,16 +6,27 @@
 % function to generate feasible x given (x,f)
 function [x,xraw] = xgenerator(x, f, bounds)
     xraw=x=mintry(x,f); % the solver, this gives x raw
-    x=parambound(x, bounds); % project to bounds, feasible guaranteed
     for i = 1:length(x)
-        % project to boundary and round here:
-        p = rand(1);
-        xl = floor(x(i));
-        f = x(i) - xl;
-        if p < 1-f; % larger chance to be rounded to the closest int
-            x(i) = xl;
+        tr = paramcheck(bounds(3,i), bounds(1,i), bounds(2,i)) % check whether need to be transformed or not
+        if bounds(3,i) == 1 % check type, int = 1, real = 0
+            x(i) = max(bounds(1,i), min(x(i), bounds(2,i))) % project to bounds
+            % stochastic round:
+            p = rand(1);
+            xl = floor(x(i));
+            f = x(i) - xl;
+            if p < 1-f; % larger chance to be rounded to the closest int
+                x(i) = xl;
+            else
+                x(i) = ceil(x(i));
+            end
         else
-            x(i) = ceil(x(i));
+            if tr
+                lb = bounds(1,i)/(1+abs(bounds(1,i))); ub = bounds(2,i)/(1+abs(bounds(2,i))); % transform bounds
+                bs = [lb, ub]; bs = sort(bs); % transform bounds
+                z = max(bs(1), min(x(i), bs(2))) % clip to transformed bounds
+                x(i) = z/(1-z)
+            else
+                x(i) = max(bounds(1,i), min(x(i), bounds(2,i))) % project to bounds
+            end
         end
     end
-    %x = min(1., max(x, 0.)); % now all variables are (0, 1), the translation is handled by Julia
