4ead5eeef2f8aeae57135d7c94b1a22674ce56dd
berylramadhian <berylramadhian@gmail.com>
2023-03-30 15:38:45 +0200

    moved penalty to xgenerator, added norm penalty for categorical var

--- VRDFON/caller_pc.m
+++ VRDFON/caller_pc.m
@@ -6,35 +6,21 @@
 %naf, nmf in percentage, e.g., 50 -> .5*max(naf("ACSF"))
 %feature name: 1=ACSF, 2=SOAP, 3=FCHL
 %model: ["ROSEMI", "KRR", "NN", "LLS", "GAK"]
-x = [.5, .5, 3, 1, 0, 0, 0, -6, -2048.0]' %  example
+%x = [.5, .5, 3, 1, 0, 0, 0, -6, -2048.0]' %  example
 path_fun = '../data/hyperparamopt/fun.txt';
-path_fun = '../test.txt';
+path_x = '../data/hyperparamopt/params.txt'
 path_bounds = '../data/hyperparamopt/bounds.txt';
 bounds = dlmread(path_bounds)
 
-
-
-%{
-a = -2; b = 2; N=3;
-xp = a + (b-a) .* rand(N,1)
-x = [0; 0; 1] + xp
-p = minprob(x)
-sum(p)
-A = rand(3,5);
-size(bounds)(1)
-%}
-bounds
-binfo = extractbound(bounds)
-x = rand(binfo(end, end), 1)
-xi = x(binfo(4,1):binfo(4,2))
-p = minprob(xi)
-z = 0;
-r = 0.990
-q = [0.; cumsum(p)]
-for i=1:length(q)-1
-    if (q(i) < r) && (r <= q(i+1))
-        z = i;
-        break
-    end
-end
-z
+f = dlmread(path_fun)(2) + 1
+x = dlmread(path_x)(2:end)'
+init.paths='minq8';     % mandatory absolute path to minq8
+init.n=length(x);              % problem dimension
+                       % For tuning or to see intermediate results, 
+                       % a nonstandard initialization may be used.
+                       % For details see mintry.m
+mintry(init);          % initialize mintry
+bm = extractbound(bounds)
+[x, xraw, fpen] = xgenerator(x, f, bounds, bm)
+xsel = xraw(bm(4,1):bm(4,2))
+p = minprob(xsel)
--- VRDFON/xgenerator.m
+++ VRDFON/xgenerator.m
@@ -4,39 +4,45 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%% xgenerator.m %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % function to generate feasible x (and xraw for tracking) given (x,f,bounds)
-% the indexing need is able to accomodate encoded x, uusing the bm matrix
-function [x,xraw] = xgenerator(x, f, bounds, bm)
+% the indexing is able to accomodate encoded x, uusing the bm matrix
+function [xnew,xraw,fpen] = xgenerator(x, f, bounds, bm)
     xraw=x=mintry(x,f); % the solver, this gives x raw
     xnew = []; % new x to be accepted by Julia
+    fpen = 0; % f penalty
     bsize = size(bounds)(2) % number of variables
     for i = 1:bsize
         % check whether need to be transformed or not:
         tr = paramcheck(bounds(3,i), bounds(1,i), bounds(2,i));
-        % check type, int = 1, real = 0, categorical = 2
-        xi = x(bm(i,1):bm(i,2)); % slice x_i given the index matrix bm
+        % slice x_i given the index matrix bm
+        xi = x(bm(i,1):bm(i,2));
+        xiraw = xraw(bm(i,1):bm(i,2));
+        % check type, int = 1, real = 0, categorical = 2:
         if bounds(3,i) == 1 
             % project to bounds:
             xi = max(bounds(1,i), min(xi, bounds(2,i)));
             % stochastic round:
-            p = rand(1);
+            r = rand(1);
             xl = floor(xi);
             f = xi - xl;
-            if p < 1-f; % larger chance to be rounded to the closest int
+            if r < 1-f; % larger chance to be rounded to the closest int
                 xi = xl;
             else
                 xi = ceil(xi);
             end
+            fpen += abs(xi - xiraw)/bounds(2,i);
         elseif bounds(3,i) == 2
             p = minprob(xi); % get probability from solving some min problem
             % choose x_i based on p:
             xi = 0;
             q = [0.; cumsum(p)];
+            r = rand(1)
             for i=1:length(q)-1
                 if (q(i) < r) && (r <= q(i+1))
                     xi = i;
                     break
                 end
             end
+            fpen += norm(p-xiraw)**2;  % compute penalty
         else
             if tr
                 lb = bounds(1,i)/(1+abs(bounds(1,i))); 
@@ -48,5 +54,7 @@ function [x,xraw] = xgenerator(x, f, bounds, bm)
                 % project to bounds:
                 xi = max(bounds(1,i), min(xi, bounds(2,i)));
             end
+            fpen += abs(xi - xiraw)/bounds(2,i);
         end
+        xnew = [xnew; xi];
     end
