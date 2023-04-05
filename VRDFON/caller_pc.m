631ee013ef3f3f70e7925220ec752df36479027c
Beryl Aribowo <berylramadhian@gmail.com>
2023-04-05 10:55:19 +0200

    added and tested x computer given arbitrary p

--- VRDFON/caller_pc.m
+++ VRDFON/caller_pc.m
@@ -13,29 +13,10 @@ path_bounds = '../data/hyperparamopt/bounds.txt';
 bounds = dlmread(path_bounds);
 
 % x = [0.5; 0.5; 6; ]
-x = [0.5; 0.5; 5.5; 1/3; 1/3; 1/3; 1/2; 1/2; 1/2; 1/2; 38; 1/5; 1/5; 1/5; 1/5; 1/5; 10.5;] % encoded x
+x = [0.5; 0.5; 5.5; 1/3; 1/3; 1/3; 1/2; 1/2; 1/2; 1/2; 38; 1/5; 1/5; 1/5; 1/5; 1/5; 10.5;]; % encoded x
 f = 100.;
 
-init.paths='minq8';     % mandatory absolute path to minq8
-init.n=length(x);              % problem dimension
-                       % For tuning or to see intermediate results, 
-                       % a nonstandard initialization may be used.
-                       % For details see mintry.m
-mintry(init);    
 
-length(x)
-bm = extractbound(bounds)
-[xin, fpen] = decode(x, bounds, bm)
-
-[xout, xin, fpen] = xgenerator(x, f, bounds, bm)
-
-intmax
-fdata = {};
-fdata{1,1} = "asd"
-
-newdata = textread(path_fun, "%s");
-if !isempty(newdata) && ~strcmp(newdata{1,1}, fdata{1,1})
-    disp("do mintry")
-else
-    disp("either the incoming data is old or it is empty")
-end
+p = [1/4, 1/2, 1/4]
+xu = 20; xl = 0;
+computex(p, xl, xu)
