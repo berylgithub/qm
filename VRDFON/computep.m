3b38a6edb186455a3f1d894011b6e9a25b1819b8
Beryl Aribowo <berylramadhian@gmail.com>
2023-04-05 11:22:22 +0200

    added p computer and joint tested with computex and minprob

---
+++ VRDFON/computep.m
@@ -0,0 +1,15 @@
+% computes p given ordered integral vector x
+function p = computep(x, xl, xu)
+    dx = xu - xl
+    n = length(x)
+    p = zeros(n+1, 1)
+    for i=1:n+1
+        i
+        if i == 1 % initial condition
+            p(i) = (x(i) - xl)/dx
+        elseif i == n+1 % boundary condition
+            p(i) = (xu - x(i-1))/dx
+        else
+            p(i) = (x(i) - x(i-1))/dx
+        end
+    end
