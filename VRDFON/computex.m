199e7b953e6b308fd99384f8b8651cba4c60a9fa
Beryl Aribowo <berylramadhian@gmail.com>
2023-04-05 10:57:18 +0200

    added and tested x computer given arbitrary p

---
+++ VRDFON/computex.m
@@ -0,0 +1,14 @@
+
+% function which computes integral x given a vector of p \in [0,1]
+function x = computex(p, xl, xu)
+    dx = xu - xl
+    n = length(p)
+    x = zeros(n-1, 1)
+    for i=1:n-1
+        if i == 1
+            x(i) = xl + p(i)*dx
+        else
+            x(i) = x(i-1) + p(i)*dx
+        end
+    end
+    x = round(x)
