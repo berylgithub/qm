e2e8b5284a1d40545cdf7db0a5de514ffd5f84a6
berylramadhian <berylramadhian@gmail.com>
2023-04-25 12:30:49 +0200

    tested repker with CGLS, much better than previous but still worse than GAK

--- RoSemi.jl
+++ RoSemi.jl
@@ -1347,16 +1347,16 @@ function testrepker()
     testids = setdiff(1:size(F, 1), centers)
     Ftrain = F[centers,:] #F[centers,:]
     Ftest = F[testids,:]
+    # repker test:
     K = comp_repker(Ftrain, Ftrain)
-    θ = K\Ered[centers]
+    θ, stat = cgls(K, Ered[centers], itmax=500, verbose=1) #θ = K\Ered[centers]
     display(mean(abs.(K*θ - Ered[centers]))*627.503)
     K = comp_repker(Ftest, Ftrain)
     display(mean(abs.(K*θ - Ered[testids]))*627.503)
+    # compare w/ gaussian atom kerneL:
     K = get_gaussian_kernel(f[centers], f[centers], [d["atoms"] for d in dataset[centers]], [d["atoms"] for d in dataset[centers]], 2048.)
-    θ = K\Ered[centers]
+    θ, stat = cgls(K, Ered[centers], itmax=500, verbose=1) #θ = K\Ered[centers]
     display(mean(abs.(K*θ - Ered[centers]))*627.503)
     K = get_gaussian_kernel(f[testids], f[centers], [d["atoms"] for d in dataset[testids]], [d["atoms"] for d in dataset[centers]], 2048.)
     display(mean(abs.(K*θ - Ered[testids]))*627.503)
-    
-
 end
