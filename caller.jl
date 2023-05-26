a13408b53dd3e87fec8b1c1a97606e1512b703c0
Beryl Aribowo <berylramadhian@gmail.com>
2023-05-26 16:40:42 +0200

    added logic for caller to be able to spawn simulators with automatic id determination

--- caller.jl
+++ caller.jl
@@ -1,6 +1,17 @@
 include("expdriver.jl")
 
+# spawns simulator ONE BY ONE to preserve paralelization, based on the available ids
 
-#testrepker()
-hyperparamopt(init=false)
 
+id = 1
+simfiles = readdir("data/hyperparamopt/sim/f/")
+while true
+    str = "sim_$id.txt"
+    if str ∉ simfiles
+        break
+    end
+    id += 1
+end
+println("spawned simulator with id = $id !!!")
+writedlm("data/hyperparamopt/sim/f/sim_$id.txt", [], "\t")
+#hyperparamopt_parallel(id; dummyfx=true, trackx = true)
