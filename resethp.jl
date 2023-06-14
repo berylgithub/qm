dd31735fac4889781b55f73f1adbb5f7ebfab965
berylgithub <berylramadhian@gmail.com>
2023-06-14 15:00:47 -0400

    added reset for hyperparamopt files

---
+++ resethp.jl
@@ -0,0 +1,17 @@
+# resets all hyperparameter optimization outputs
+
+paths = ["data/hyperparamopt/fun.txt",
+        "data/hyperparamopt/params.txt",
+        "data/hyperparamopt/raw_params.txt"
+        "data/hyperparamopt/flist.txt",
+        "data/hyperparamopt/xlist.txt",
+        "data/hyperparamopt/xrawlist.txt",
+        "data/hyperparamopt/tracker.txt",
+        "data/hyperparamopt/sim/sim_tracker.txt"]
+
+for pt ∈ paths
+    run(`rm $pt`)
+    run(`touch $pt`)
+end
+run(`rm -r data/hyperparamopt/sim/f/*`)
+run(`rm -r data/hyperparamopt/sim/x/*`)
