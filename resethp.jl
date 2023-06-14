6dfa48d179b56a6ba9992e70ad9447e2fe1d2b6f
berylgithub <berylramadhian@gmail.com>
2023-06-14 15:38:26 -0400

    bugfix param

--- resethp.jl
+++ resethp.jl
@@ -8,12 +8,13 @@ paths = ["data/hyperparamopt/fun.txt",
         "data/hyperparamopt/xrawlist.txt",
         "data/hyperparamopt/tracker.txt",
         "data/hyperparamopt/sim/sim_tracker.txt"]
-
+# recreate files:
 for pt ∈ paths
     rm(pt)
     io = open(pt, "w")
     close(io)
 end
+# remove only:
 files = vcat(readdir("data/hyperparamopt/sim/f/"), readdir("data/hyperparamopt/sim/x/"))
 for f ∈ files
     rm(f)
