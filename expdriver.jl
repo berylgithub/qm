3c02e2f9a2f8d42204f29dcfa0f488bfc3b29e88
berylgithub <berylramadhian@gmail.com>
2023-10-17 15:55:24 -0400

    try custom CMBDF center training

--- expdriver.jl
+++ expdriver.jl
@@ -702,6 +702,8 @@ function sanity_check()
     println("SANITY CHECK DONE!")
 end
 
+
+
 function test_mainobj()
     # simulate input parameters:
     sim_id = 1
@@ -731,6 +733,46 @@ function test_mainobj()
     fx = main_obj
     f = fx(E, dataset, DFs, Fs, centers, idtrains, x; sim_id = "_$sim_id")
     writedlm("test_mainobj.txt", f)
+end
+
+"""
+since we figured out that CMBDF gives the best MAE ~4.5 kcal/mol with selection where CMBDF WAS NOT INCLUDED, so now we want to see if the data seleciton uses MBDF what would happen
+"""
+function main_custom_CMBDF_train()
+    f = load("data/CMBDF.jld", "data")
+    nrow = length(f)
+    # try using flatten instead of the usual sum, feels like summing causes some information lost:
+    ncol = 29*40
+    F = zeros(Float64, nrow, ncol)
+    for i ∈ 1:nrow
+        F[i,eachindex(f[i])] = vec(transpose(f[i])) # flatten
+    end
+    Random.seed!(777)
+    centerss = set_cluster(F, 200; universe_size = 1000, num_center_sets = 100)
+    display(Int.(transpose(reduce(hcat, centerss))))
+    writedlm("data/custom_CMBDF_centers.txt", centerss)
+    # run it all through the base "main_obj":
+    # spawn all memory dependednt data:
+    sim_id = "custom_CMBDF_centers"
+    x = [0, 0, 0, 0, 0, 0, 10, 10, 10, 0, 0, 50, 50, 3, 5, 0, 0, 5, 11, 2] # current best conf found w.r.t the current hyperparameter space, 5.03 kcal/mol
+    # inside functions:
+    dataset = load("data/qm9_dataset.jld", "data") # dataset info
+    E = vec(readdlm("data/energies.txt")) # base energy
+    Fa = load("data/atomref_features.jld", "data") # DA feature
+    Fb = load("data/featuresmat_bonds_qm9_post.jld", "data") # DB
+    Fn = load("data/featuresmat_angles_qm9_post.jld", "data") # DN
+    Ft = load("data/featuresmat_torsion_qm9_post.jld", "data") # DT
+    DFs = [Fa, Fb, Fn, Ft]
+    feat_paths = ["data/ACSF_51.jld", "data/SOAP.jld", "data/FCHL19.jld", "data/MBDF.jld", "data/CMBDF.jld"]
+    Fs = [[],[],[],[],load(feat_paths[5], "data")]
+    fx = main_obj
+    for i ∈ axes(centerss, 1) # centers, idtrains, idtests:
+        centers = centerss[i,:]
+        idtrains = centers[1:100]
+        fobj = fx(E, dataset, DFs, Fs, centers, idtrains, x; sim_id = "_$sim_id")
+        strinput = string.([fobj])
+        writestringline(strinput, "result/deltaML/MAE_custom_CMBDF_centers.txt"; mode="a")
+    end
 end
 
 
