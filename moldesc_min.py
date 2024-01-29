cabddba5504ecb9ecfb0d7822b6d43e4d96984a5
Beryl Aribowo <berylramadhian@gmail.com>
2024-01-29 15:33:48 +0100

    changed abs dir => rel dir

--- moldesc_min.py
+++ moldesc_min.py
@@ -3,8 +3,8 @@ import numpy as np
 import cMBDF
 
 def extract_MBDF():
-    print(np.load("/users/baribowo/Code/Julia/qm/data/test_ncs.npy", allow_pickle=True))
-    print(np.load("/users/baribowo/Code/Julia/qm/data/test_coors.npy", allow_pickle=True))
+    print(np.load("data/test_ncs.npy", allow_pickle=True))
+    print(np.load("data/test_coors.npy", allow_pickle=True))
 
 
 extract_MBDF()
