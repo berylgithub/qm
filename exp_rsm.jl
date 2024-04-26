a05a60ab6f8b2bd320e1c1bf527c26ae8d8296e8
Beryl Aribowo <berylramadhian@gmail.com>
2024-04-26 18:35:04 +0200

    separated new rsm runs to "exp_rsm.jl"

---
+++ exp_rsm.jl
@@ -0,0 +1,178 @@
+include("RoSemi.jl")
+include("expdriver.jl")
+include("utils.jl")
+
+using DelimitedFiles
+using Random, Combinatorics
+using MLBase
+
+using Plots
+using PlotlyJS, Polynomials
+const jplot = PlotlyJS.plot
+const jscatter = PlotlyJS.scatter
+const jsavefig = PlotlyJS.savefig
+
+
+"""
+converts distance to r^k/(r^k+r0^k), k=1,2,3, r0 ≈ req
+"""
+function rdist(r,r0;c=1)
+    return r^c/(r^c + r0^c)
+end
+
+"""
+converts distance to e^{-r/r0}
+"""
+function edist(r,r0)
+    return exp(-r/r0)
+end
+
+"""
+figure out the equilibrium distances for each dataset
+"""
+function main_eq_dist()
+    data = load("data/smallmol/hxoy_data.jld", "data")
+    # check using the lowest energy query:
+    mols = map(d-> d["mol"], data)
+    umols = unique(mols)
+    for smol in umols
+        reqs = []
+        for (i,d) in enumerate(data)
+            if d["mol"] == smol
+                veqid = argmin(d["V"])
+                req = d["R"][veqid]
+                push!(reqs, req)
+                println([req, d["mol"],i])
+            end
+        end
+    end
+    # include some indices:
+    iids = [1,2,3,11,12,13,14,15,16,17,20,21,23,24,25]
+    data = data[iids]
+    mols = map(d-> d["mol"], data)
+    umols = unique(mols)
+    for smol in umols
+        reqs = []
+        for (i,d) in enumerate(data)
+            if d["mol"] == smol
+                veqid = argmin(d["V"])
+                req = d["R"][veqid]
+                d["req"] = req
+                push!(reqs, req)
+                println([req, d["mol"],i])
+            end
+        end
+    end
+    display(map(d->d["req"], data))
+    # double check using visual:
+    for d in data
+        if d["mol"] == "O2"
+            d["R"] = d["R"][1:end-1]; d["V"] = d["V"][1:end-1] # remove end outliers
+            d["note"] = "removed (R,V)[end], outlier at faraway distance"
+            p = jplot(jscatter(x=d["R"][1:end-1], y=d["V"][1:end-1], mode="lines"))
+            display(d["mol"])
+            display(p)
+            display(d)
+        end
+    end
+    # save reselected data:
+    save("data/smallmol/hxoy_data_req.jld", "data", data) 
+end
+
+"""
+rosemi wrapper fitter, can choose either with kfold or usequence (if kfold = true then pcs isnt used)   
+    - F = feature vector (or matrix)
+    - E = target energy vector
+    - kfold = use kfold or usequence
+    - k = number of folds
+    - pcs = percentage of centers
+    - ptr = percentage of trainings
+"""
+function rosemi_fitter(F, E; kfold = true, k = 5, pcs = 0.8, ptr = 0.5, n_basis=4, λ = 0., force=true)
+    ndata = length(E)
+    folds = collect(Kfold(ndata, k)) # for rosemi, these guys are centers, not "training set"
+    MAEs = []; RMSEs = []; RMSDs = []; t_lss = []; t_preds = []
+    for (i,fold) in enumerate(folds)
+        # fitter:
+        ϕ, dϕ = extract_bspline_df(F', n_basis; flatten=true, sparsemat=true)
+        fold = shuffle(fold); centers = fold; lenctr = length(centers) # the center ids
+        trids = fold[1:Int(round(ptr*lenctr))] # train ids (K)
+        uids = setdiff(fold, trids) # unsupervised ids (U)
+        tsids = setdiff(1:ndata, fold)
+        D = fcenterdist(F, centers) .+ λ # temp fix for numericals stability
+        bsize = max(1, Int(round(0.25*length(tsids)))) # to avoid bsize=0
+        MAE, RMSE, RMSD, t_ls, t_pred = fitter(F', E, D, ϕ, dϕ, trids, centers, uids, tsids, size(F, 2), "test", bsize, 900, get_rmse=true, force=force)    
+        MAE = MAE/627.503 ## MAE in energy input's unit
+        # store results:
+        push!(MAEs, MAE); push!(RMSEs, RMSE); push!(RMSDs, RMSD); push!(t_lss, t_ls); push!(t_preds, t_pred); 
+    end
+    return MAEs, RMSEs, RMSDs, t_lss, t_preds 
+end
+
+
+"""
+objective function for hyperparam opt 
+"""
+function rosemi_fobj(R, E, req; kfold=true, force=true, c=1, n_basis=4, ptr=0.5)
+    F = rdist.(R, req; c=c)
+    MAEs, RMSEs, RMSDs, t_lss, t_preds  = rosemi_fitter(F, E; kfold=kfold, k = 5, n_basis=n_basis, ptr=ptr, force=force)
+    return mean(RMSEs) # would mean() be a better metric here? or min() is preferrable?
+end
+
+
+"""
+rerun of HxOy using ROSEMi
+"""
+function main_rosemi_hxoy(;force=true, c=1, n_basis=4, ptr=0.5)
+    Random.seed!(603)
+    # pair HxOy fitting:
+    data = load("data/smallmol/hxoy_data_req.jld", "data") # load hxoy
+    # do fitting for each dataset:
+    # for each dataset split kfold
+    # possibly rerun with other models?
+    ld_res = []
+    for i ∈ eachindex(data)[1:1]
+        # extract data:
+        d = data[i]
+        req = d["req"]
+        F = rdist.(d["R"], req, c=c) #edist.(d["R"], req) # convert distance features
+        E = d["V"]
+        println([d["mol"], d["author"], d["state"]])
+        MAEs, RMSEs, RMSDs, t_lss, t_preds  = rosemi_fitter(F, E; kfold=true, k = 5, n_basis=n_basis, ptr=ptr, force=force)
+        display([MAEs, RMSEs, RMSDs, t_lss, t_preds ])
+        println("RMSE = ", RMSEs)
+        # result storage:
+        d_res = Dict()
+        d_res["MAE"] = MAEs; d_res["RMSE"] = RMSEs; d_res["RMSD"] = RMSDs; d_res["t_train"] = t_lss; d_res["t_test"] = t_preds;
+        d_res["mol"] = d["mol"]; d_res["author"] = d["author"]; d_res["state"] = d["state"]; 
+        display(d_res)
+        push!(ld_res, d_res)
+    end
+    #save("result/hdrsm_[$force].jld", "data", ld_res)
+end
+
+
+"""
+rerun of Hn, 3 ≤ n ≤ 5 molecules using ROSEMI
+"""
+function main_rosemi_hn(;force=true)
+    Random.seed!(603)
+    data = load("data/smallmol/hn_data.jld", "data")
+    ld_res = []
+    for i ∈ eachindex(data)
+        λ = 0.
+        d = data[i]; F = d["R"]; E = d["V"]
+        if d["mol"] == "H5" # for H5, add "regularizer"
+            λ = 1e-8
+        end
+        println(d["mol"])
+        MAEs, RMSEs, RMSDs, t_lss, t_preds  = rosemi_fitter(F, E; kfold=true, k = 5, n_basis=4, ptr=0.5, λ = λ, force=force) # λ = 1e-8
+        display([MAEs, RMSEs, RMSDs, t_lss, t_preds])
+        # result storage:
+        d_res = Dict()
+        d_res["MAE"] = MAEs; d_res["RMSE"] = RMSEs; d_res["RMSD"] = RMSDs; d_res["t_train"] = t_lss; d_res["t_test"] = t_preds;
+        d_res["mol"] = d["mol"];
+        push!(ld_res, d_res)
+    end
+    save("result/hn_rosemi_rerun_[$force].jld", "data", ld_res) #save("result/h5_rosemi_rerun_unstable.jld", "data", ld_res) 
+end
